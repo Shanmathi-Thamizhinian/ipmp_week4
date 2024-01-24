6062d92072757f1e1f2b7e9194e72c9b2e9a8490
Shanmathi Thamizhinian <122631445+Shanmathi-Thamizhinian@users.noreply.github.com>
2024-01-24 08:56:31 +0530

    Create d1_q1.cpp

---
+++ d1_q1.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> firstAndLast(vector<int> &arr, int n, int x) {
+        int fo=-1,lo=-1;
+        vector<int> ans;
+        int low=0,high=n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==x){
+                lo=mid;
+                low=mid+1;
+            }
+            else if(arr[mid]<x){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        low=0,high=n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==x){
+                fo=mid;
+                high=mid-1;
+            }
+            else if(arr[mid]<x){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        if(fo==-1 && lo==-1){
+            return {-1};
+        }
+        ans.push_back(fo);
+        ans.push_back(lo);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+
+        Solution obj;
+        vector<int> ans= obj.firstAndLast(arr, n, x) ;
+        for(int i:ans){
+            cout<<i<<" ";
+        }
+        cout<< endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
